913988f76325328603d9e0f9c243d0f3d7fc7c43
Martin Weis <Martin.Weis.newsadress@gmx.de>
2025-03-26 22:01:10 +0100

    LSE01, SE01-LB parseFloat after toFixed and indentation checked

--- LSE01/LSE01_TTN Decoder_V1.1.4.js
+++ LSE01/LSE01_TTN Decoder_V1.1.4.js
@@ -2,22 +2,22 @@ function Decoder(bytes, port) {
   // Decode an uplink message from a buffer
   // (array) of bytes to an object of fields.
 	var value=(bytes[0]<<8 | bytes[1]) & 0x3FFF;
-	var batV=value/1000;//Battery,units:V
+	var batV=parseFloat((value/1000).toFixed(2));//Battery,units:V
 
 	value=bytes[2]<<8 | bytes[3];
 	if(bytes[2] & 0x80)
 	{value |= 0xFFFF0000;}
-	var temp_DS18B20=(value/10).toFixed(2);//DS18B20,temperature
+	var temp_DS18B20=parseFloat((value/10).toFixed(2));//DS18B20,temperature
    
 	value=bytes[4]<<8 | bytes[5];
-	var water_SOIL=(value/100).toFixed(2);//water_SOIL,Humidity,units:%
+	var water_SOIL=parseFloat((value/100).toFixed(2));//water_SOIL,Humidity,units:%
    
 	value=bytes[6]<<8 | bytes[7];
 	var temp_SOIL;
 	if((value & 0x8000)>>15 === 0)
-		temp_SOIL=(value/100).toFixed(2);//temp_SOIL,temperature
+		temp_SOIL=parseFloat((value/100).toFixed(2));//temp_SOIL,temperature
 	else if((value & 0x8000)>>15 === 1)
-		temp_SOIL=((value-0xFFFF)/100).toFixed(2);
+		temp_SOIL=parseFloat(((value-0xFFFF)/100).toFixed(2));
    
 	value=bytes[8]<<8 | bytes[9];
 	var conduct_SOIL=value;//conduct_SOIL,conductivity,units:uS/cm,max:65535	uS/cm
